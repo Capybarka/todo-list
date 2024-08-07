b2263c50f9a32b8f5ad3b398107f2cc3eee3d678
Capybarka <marrinna03@mail.ru>
2024-08-07 18:53:08 +0300

    #

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+document.querySelector('.btn-addTask').addEventListener('click', () => {
+    let modal = document.querySelector('.modal');
+    modal.classList.remove('display-none');
+})
+
